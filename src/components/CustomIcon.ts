4dc951fd1a37c16420228eb6f1fbdb80fc2fc0d6
Kasun Umesh <kasunumesh38@gmail.com>
2024-10-28 01:03:57 +0530

    init project

---
+++ src/components/CustomIcon.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
+import icoMoonConfig from '../../selection.json';
+export default createIconSetFromIcoMoon(icoMoonConfig);
